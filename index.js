b8cba2b01175351b086888ca30e43e563175b44b
Phuoc <phuoc.dt182724@sis.hust.edu.vn>
2022-03-30 15:22:57 +0700

    output query attributes by using url.parse() and res.write() method

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
+
 
 //create a server using 'http' modules
-const server = http.createServer((req,res) =>{
-    res.writeHead(200, 'content-type: text');
-    res.write('<h1>Hello world!!</h1>');
-    const urlData = url.parse(req.url,true);
-    console.log(urlData);
-    
-    res.end();
+const server = http.createServer((req, res) => {
+  res.writeHead(200, "content-type: text");
+  res.write("<h1>Hello world!!</h1>");
+  let urlData = url.parse(req.url, true);
+  //console.log(urlData.query);
+  res.write(`param animal: ${urlData.query.animal} <br>`);
+  res.write(`param color: ${urlData.query.color} <br>`);
+  res.end();
 });
 
 server.listen(3000);
